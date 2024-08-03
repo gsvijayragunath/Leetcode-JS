adef180b09a7a2d1f032bd2375598c717ddb02c7
VIJAY RAGUNATH GS <139546456+gsvijayragunath@users.noreply.github.com>
2024-08-03 08:25:52 +0530

    Time: 60 ms (35.22%), Space: 49.7 MB (80.14%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let res = [];
+    for(i=0;i<nums1.length;i++){
+    if(nums2.includes(nums1[i])){
+    res.push(nums1[i])
+  }
+}
+return  Array.from(new Set(res));
+};
