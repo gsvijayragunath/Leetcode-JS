324ec1e06aa631f1f773823520304bf2007e27e1
VIJAY RAGUNATH GS <139546456+gsvijayragunath@users.noreply.github.com>
2025-03-05 12:18:25 +0530

    Time: 1 ms (89.73%), Space: 54.7 MB (15.49%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let hashmap = new Map();
+
+    for(let i =0;i<nums.length;i++){
+    let complement = target -  nums[i];
+        if(hashmap.has(complement)){
+            return [hashmap.get(complement),i]
+        }
+        hashmap.set(nums[i],i)
+    }
+    return []
+};
