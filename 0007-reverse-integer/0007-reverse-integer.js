df9a7e6ebd9a1defa94fbd16397e2565971b2cd4
VIJAY RAGUNATH GS <139546456+gsvijayragunath@users.noreply.github.com>
2025-03-21 20:15:04 +0530

    Time: 52 ms (75.18%), Space: 54.9 MB (38.63%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -2,20 +2,18 @@
  * @param {number} x
  * @return {number}
  */
-
-    
 var reverse = function(x) {
-  const maxInt = Math.pow(2, 31) - 1;
-  const minInt = -Math.pow(2, 31);
-  let rev = 0;
+    let bit = Math.pow(2,31) -1 ;
+    let rev = x.toString().split("").reverse().join('');
+    let result = parseInt(rev);
 
-  while (x !== 0) {
-    const pop = x % 10;
-    x = parseInt(x / 10);
-    if (rev > maxInt / 10 || (rev === maxInt / 10 && pop > 7)) return 0;
-    if (rev < minInt / 10 || (rev === minInt / 10 && pop < -8)) return 0;
-    rev = rev * 10 + pop;
-  }
+    if(result > (bit) || result <-(bit)){
+        return 0;
+    }
 
-  return rev;
+    if(x<0){
+        return -result
+    }else{
+        return result
+    }
 };
